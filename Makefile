a42c7ef8b41028f0fa2baec89e6d5ded534ee542
Christopher Biscardi <chris@christopherbiscardi.com>
2020-12-13 23:02:52 -0800

    set up commonmark integration tests

---
+++ Makefile
@@ -0,0 +1,2 @@
+fetch-spec-tests:
+	@wget https://spec.commonmark.org/0.29/spec.json -O tests/fixtures.json
---
+++ tests/commonmark.rs
@@ -0,0 +1,28 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(datatest::runner)]
+
+use serde::Deserialize;
+use std::fmt;
+use mdx::*;
+
+#[derive(Deserialize)]
+struct CommonmarkTestCase {
+  markdown: String,
+  html: String,
+  example: usize,
+  start_line: usize,
+  end_line: usize,
+  section: String
+}
+
+impl fmt::Display for CommonmarkTestCase {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "example: {} ({})", self.example, self.section)
+    }
+}
+
+#[cfg(feature = "commonmark")]
+#[datatest::data("tests/fixtures.json")]
+fn sample_test(case: CommonmarkTestCase) {
+  assert_eq!(case.html, case.markdown);
+}
